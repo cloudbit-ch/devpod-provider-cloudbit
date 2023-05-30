085ec467c520bfa7da5f8a858383978dfff6bcac
Bethuel <bethuelmbaga12@gmail.com>
2023-05-31 00:28:24 +0300

    get compute instance by name

--- pkg/cloudbit/cloudbit.go
+++ pkg/cloudbit/cloudbit.go
@@ -38,8 +38,13 @@ func (c *Cloudbit) Create(ctx context.Context, req compute.ServerCreate) error {
 	return nil
 }
 
-func (c *Cloudbit) Stop(ctx context.Context, instanceID int) error {
-	_, err := c.computeService.Perform(ctx, instanceID, compute.ServerPerform{Action: "stop"})
+func (c *Cloudbit) Stop(ctx context.Context, machineID string) error {
+	server, err := c.GetInstanceByName(ctx, machineID)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.computeService.Perform(ctx, server.ID, compute.ServerPerform{Action: "stop"})
 	if err != nil {
 		return err
 	}
@@ -47,8 +52,13 @@ func (c *Cloudbit) Stop(ctx context.Context, instanceID int) error {
 	return nil
 }
 
-func (c *Cloudbit) Status(ctx context.Context, instanceID int) (client.Status, error) {
-	server, err := c.computeService.Get(ctx, instanceID)
+func (c *Cloudbit) Status(ctx context.Context, machineID string) (client.Status, error) {
+	server, err := c.GetInstanceByName(ctx, machineID)
+	if err != nil {
+		return "", err
+	}
+
+	server, err = c.computeService.Get(ctx, server.ID)
 	if err != nil {
 		return client.StatusNotFound, err
 	}
@@ -65,6 +75,26 @@ func (c *Cloudbit) Status(ctx context.Context, instanceID int) (client.Status, e
 	return client.StatusNotFound, nil
 }
 
-func (c *Cloudbit) Delete(ctx context.Context, instanceID int) error {
-	return c.computeService.Delete(ctx, instanceID, true)
+func (c *Cloudbit) Delete(ctx context.Context, machineID string) error {
+	server, err := c.GetInstanceByName(ctx, machineID)
+	if err != nil {
+		return err
+	}
+
+	return c.computeService.Delete(ctx, server.ID, true)
+}
+
+func (c *Cloudbit) GetInstanceByName(ctx context.Context, machineID string) (compute.Server, error) {
+	serverList, err := c.computeService.List(ctx, goclient.Cursor{})
+	if err != nil {
+		return compute.Server{}, err
+	}
+
+	for _, server := range serverList.Items {
+		if server.Name == machineID {
+			return server, nil
+		}
+	}
+
+	return compute.Server{}, errors.New("instance name not found")
 }

a1628b58c0d6e6c7a769d43462ee5548daf63bde
Bethuel <bethuelmbaga12@gmail.com>
2023-05-31 00:37:14 +0300

    implement provider stop command

--- cmd/stop.go
+++ cmd/stop.go
@@ -1,11 +1,46 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"github.com/cloudbit/devpod-provider-cloudbit/pkg/cloudbit"
+	"github.com/cloudbit/devpod-provider-cloudbit/pkg/options"
+	"github.com/loft-sh/devpod/pkg/client"
+	"github.com/loft-sh/devpod/pkg/log"
+	"github.com/spf13/cobra"
+	"time"
+)
 
 var stopCmd = &cobra.Command{
 	Use:   "stop",
 	Short: "Stop an instance",
 	RunE: func(_ *cobra.Command, args []string) error {
+		options, err := options.FromEnv(false)
+		if err != nil {
+			return err
+		}
+
+		cloudBitClient := cloudbit.NewCloudbit(options.Token)
+		err = cloudBitClient.Stop(context.Background(), options.MachineID)
+		if err != nil {
+			return err
+		}
+
+		// wait until stopped
+		for {
+			status, err := cloudBitClient.Status(context.Background(), options.MachineID)
+			if err != nil {
+				log.Default.Errorf("Error retrieving instance status: %v", err)
+				break
+			}
+
+			if status == client.StatusStopped {
+				break
+			}
+
+			// make sure we don't spam
+			time.Sleep(time.Second)
+		}
+
 		return nil
 	},
 }

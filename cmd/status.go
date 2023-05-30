ed6fd491ca6ca08ad25a7b1faaa348936208e703
Bethuel <bethuelmbaga12@gmail.com>
2023-05-31 00:31:57 +0300

    implement provider status command

--- cmd/status.go
+++ cmd/status.go
@@ -1,11 +1,29 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+	"github.com/cloudbit/devpod-provider-cloudbit/pkg/cloudbit"
+	"github.com/cloudbit/devpod-provider-cloudbit/pkg/options"
+	"github.com/spf13/cobra"
+	"os"
+)
 
 var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Retrieve the status of an instance",
 	RunE: func(_ *cobra.Command, args []string) error {
-		return nil
+		options, err := options.FromEnv(false)
+		if err != nil {
+			return err
+		}
+
+		status, err := cloudbit.NewCloudbit(options.Token).Status(context.Background(), options.MachineID)
+		if err != nil {
+			return err
+		}
+
+		_, err = fmt.Fprint(os.Stdout, status)
+		return err
 	},
 }

3dcb512677d132f94ca19a83792e8edb6632c0b3
Bethuel <bethuelmbaga12@gmail.com>
2023-05-31 00:44:06 +0300

    implement provider delete command

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,13 +1,46 @@
 package cmd
 
 import (
+	"context"
+	"github.com/cloudbit/devpod-provider-cloudbit/pkg/cloudbit"
+	"github.com/cloudbit/devpod-provider-cloudbit/pkg/options"
+	"github.com/loft-sh/devpod/pkg/client"
+	"github.com/loft-sh/devpod/pkg/log"
 	"github.com/spf13/cobra"
+	"time"
 )
 
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "Delete an instance",
 	RunE: func(_ *cobra.Command, args []string) error {
+		options, err := options.FromEnv(false)
+		if err != nil {
+			return err
+		}
+
+		cloudBitClient := cloudbit.NewCloudbit(options.Token)
+		err = cloudBitClient.Delete(context.Background(), options.MachineID)
+		if err != nil {
+			return err
+		}
+
+		// wait until deleted
+		for {
+			status, err := cloudBitClient.Status(context.Background(), options.MachineID)
+			if err != nil {
+				log.Default.Errorf("Error retrieving instance status: %v", err)
+				break
+			}
+
+			if status == client.StatusNotFound {
+				break
+			}
+
+			// make sure we don't spam
+			time.Sleep(time.Second)
+		}
+
 		return nil
 	},
 }

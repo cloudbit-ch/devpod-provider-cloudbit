0af7c5fdaaa49e884df94f67b29e4f94360d9ffd
Bethuel <bethuelmbaga12@gmail.com>
2023-06-01 21:36:58 +0300

    implement provider start command

--- cmd/start.go
+++ cmd/start.go
@@ -1,11 +1,26 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"github.com/cloudbit/devpod-provider-cloudbit/pkg/cloudbit"
+	"github.com/cloudbit/devpod-provider-cloudbit/pkg/options"
+	"github.com/spf13/cobra"
+)
 
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Start an instance",
 	RunE: func(_ *cobra.Command, args []string) error {
-		return nil
+		options, err := options.FromEnv(false)
+		if err != nil {
+			return err
+		}
+
+		req, err := buildCreateInstanceRequest(options)
+		if err != nil {
+			return err
+		}
+
+		return cloudbit.NewCloudbit(options.Token).Create(context.Background(), req)
 	},
 }
